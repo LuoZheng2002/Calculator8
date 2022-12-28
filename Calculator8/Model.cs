67ccd1494f20bae1b24f805a1b14a0053c16139b
luo20 <luo20@DESKTOP-B0PREDL>
2022-12-28 16:57:53 +0800

    添加项目文件。

---
+++ Calculator8/Model.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator8
+{
+    internal class Model
+    {
+        public int Adder1 { get; set; }
+        public int Adder2 { get; set; }
+        public int Sum { get; set; }
+        public event Action SumChanged;
+        public void Add()
+        {
+            Sum = Adder1 + Adder2;
+            Console.WriteLine("Added!");
+            SumChanged?.Invoke();
+        }
+    }
+}
---
+++ Calculator8/ViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Calculator8
+{
+    internal class ViewModel:INotifyPropertyChanged
+    {
+        private Model _model;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        void OnSumChanged()
+        {
+            OnPropertyChanged(nameof(Sum));
+        }
+        public int Adder1
+        {
+            get { return _model.Adder1; }
+            set { _model.Adder1 = value; }
+        }
+        public int Adder2
+        {
+            get { return _model.Adder2; }
+            set { _model.Adder2 = value; }
+        }
+        public int Sum
+        {
+            get { return _model.Sum; }
+            set { _model.Sum = value; }
+        }
+        public ICommand AddCommand { get; }
+        public ViewModel()
+        {
+            _model = new Model();
+            AddCommand = new AddCommand();
+            ((AddCommand)AddCommand).Called += _model.Add;
+            _model.SumChanged += OnSumChanged;
+        }
+    }
+    internal class AddCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+        public event Action Called;
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            Called?.Invoke();
+        }
+    }
+}
